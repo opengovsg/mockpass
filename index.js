511e5908b485deace64d84be1965713be8b34927
LoneRifle <LoneRifle@users.noreply.github.com>
2018-09-07 14:31:01 +0800

    Point SingPass and CorpPass to separate assert endpoints
    * Refactor express configuration so that it can point to separate assert
      endpoints from the service provider, one for SingPass and another for CorpPass
    * SAML Artifact is constructed from the identity provider's partner id, not the service provider's
    * Move defaults for service provider stuff for crypto to index file

--- index.js
+++ index.js
@@ -5,16 +5,24 @@ const { config } = require('./lib/express')
 
 const PORT = process.env.PORT || 5156
 
-const assertEndpoint = process.env.ASSERT_ENDPOINT
-if (!assertEndpoint) {
-  throw new Error('ASSERT_ENDPOINT must be set')
+if (!process.env.SINGPASS_ASSERT_ENDPOINT && !process.env.CORPPASS_ASSERT_ENDPOINT) {
+  throw new Error('Either SINGPASS_ASSERT_ENDPOINT or CORPPASS_ASSERT_ENDPOINT must be set')
 }
 
 const app = config(express(), {
-  assertEndpoint,
   serviceProviderPaths: {
-    cert: process.env.SERVICE_PROVIDER_CERT_PATH,
-    pubKey: process.env.SERVICE_PROVIDER_PUB_KEY,
+    cert: process.env.SERVICE_PROVIDER_CERT_PATH || './static/certs/server.crt',
+    pubKey: process.env.SERVICE_PROVIDER_PUB_KEY || './static/certs/key.pub',
+  },
+  idpConfig: {
+    singPass: {
+      id: process.env.SINGPASS_IDP_ID || 'https://saml-internet.singpass.gov.sg/FIM/sps/SingpassIDPFed/saml20',
+      assertEndpoint: process.env.SINGPASS_ASSERT_ENDPOINT,
+    },
+    corpPass: {
+      id: process.env.CORPPASS_IDP_ID || 'https://saml.corppass.gov.sg/FIM/sps/CorpIDPFed/saml20',
+      assertEndpoint: process.env.CORPPASS_ASSERT_ENDPOINT,
+    },
   },
   showLoginPage: process.env.SHOW_LOGIN_PAGE === 'true',
 })
