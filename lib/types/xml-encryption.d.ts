de16945e98a66d5a58cbe364f92f00678fa12b1c
Antariksh Mahajan <antarikshmahajan@gmail.com>
2020-10-08 18:04:15 +0800

    feat: add types for xml-encryption

---
+++ lib/types/xml-encryption.d.ts
@@ -0,0 +1,40 @@
+/**
+ * Adapted from https://github.com/tngan/samlify/blob/master/@types/xml-encryption.d.ts
+ */
+
+declare module 'xml-encryption' {
+  export interface EncryptOptions {
+    rsa_pub: string | Buffer
+    pem: string | Buffer
+    encryptionAlgorithm: string
+    keyEncryptionAlgorithm: string
+    input_encoding?: string
+  }
+  export interface DecryptOptions {
+    key: string | Buffer
+  }
+  export interface Callback {
+    (err: Error, result): unknown
+  }
+  export function encrypt(
+    content: string,
+    options: EncryptOptions,
+    callback: Callback,
+  ): string
+  export function encryptKeyInfo(
+    symmetricKey: string,
+    options: EncryptOptions,
+    callback: Callback,
+  ): string
+  export function decrypt(
+    xml: string | Document,
+    options: DecryptOptions,
+    callback: Callback,
+  ): string
+  export function decryptKeyInfo(
+    doc: string | Document,
+    options: DecryptOptions,
+  ): string
+  const _default: { decrypt; encrypt; decryptKeyInfo; encryptKeyInfo }
+  export default _default
+}
