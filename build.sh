94444792fcff7ff9b468de97b7332774970b98a8
David <david.t@cashshield.com>
2021-11-03 14:51:28 +0800

    chore: added dockerignore and the build.sh to redeploy new images

--- build.sh
+++ build.sh
@@ -7,3 +7,5 @@ docker push 722747748666.dkr.ecr.ap-southeast-1.amazonaws.com/notarise-mockpass:
 
 docker build -t 722747748666.dkr.ecr.ap-southeast-1.amazonaws.com/notarise-mockpass:$EPOCH .
 docker push 722747748666.dkr.ecr.ap-southeast-1.amazonaws.com/notarise-mockpass:$EPOCH
+
+aws ecs update-service --cluster notarise-mockpass --service arn:aws:ecs:ap-southeast-1:722747748666:service/notarise-mockpass/notarise-mockpass-service --force-new-deployment
